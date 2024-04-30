8a065e7ca6b7c547a38dd14bdf25f2fd076654ab
AnicetChiza <chizaanicet@gmail.com>
2024-04-30 20:43:39 +0200

    add to my repository

--- javascript/app.js
+++ javascript/app.js
@@ -26,3 +26,16 @@ function startValueAnimation() {
 
 // Événement de chargement de la page
 document.addEventListener('DOMContentLoaded', startValueAnimation);
+
+//Header changing background
+jQuery(function($) {
+    const $header = $('header');
+    $(window).scroll(function(event) {
+        const $current = $(this).scrollTop();
+        if ($current > 560) {
+            $header.addClass('header-color');
+        } else {
+            $header.removeClass('header-color');
+        }
+    });
+});
