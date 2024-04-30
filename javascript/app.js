5907e6bd911d8258ea7d0d94cb88a8b06f83779d
AnicetChiza <chizaanicet@gmail.com>
2024-04-30 20:47:15 +0200

    add to my repository

--- javascript/app.js
+++ javascript/app.js
@@ -32,7 +32,7 @@ jQuery(function($) {
     const $header = $('header');
     $(window).scroll(function(event) {
         const $current = $(this).scrollTop();
-        if ($current > 560) {
+        if ($current > 150) {
             $header.addClass('header-color');
         } else {
             $header.removeClass('header-color');
